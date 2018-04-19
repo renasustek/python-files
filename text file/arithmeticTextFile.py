4ed64f194dbd9824742a1e20035a93f9191ab2f9
renasustek <renasustek@gmail.com>
2018-04-19 21:10:24 +0100

    Correct condition

--- text file/arithmeticTextFile.py
+++ text file/arithmeticTextFile.py
@@ -1,11 +1,8 @@
 file = open("arithmetic.txt", "w")
-
 whichOperation = input("Which Operation do you want: ")
-
 number1 = input("Enter number 1: ")
 number2 = input("Enter number 2: ")
 file.write(number1 + whichOperation + number2)
-
 file.close()
 
 file = open("arithmetic.txt", "r")
@@ -14,21 +11,23 @@ answer = None
 
 file.close()
 
-if whichOperation in line and whichOperation=="+":
-    data = line.split(whichOperation)
-    answer = int(data[0]) + int(data[1])
-elif whichOperation in line and whichOperation=="-":
-    data = line.split(whichOperation)
-    answer = int(data[0]) - int(data[1])
-elif whichOperation in line and whichOperation=="*":
-    data = line.split(whichOperation)
-    answer = int(data[0]) * int(data[1])
-elif whichOperation in line and whichOperation=="/":
-    data = line.split(whichOperation)
-    answer = int(data[0]) / int(data[1])
-
-
+if whichOperation in line:
+    if whichOperation == "+":
+        data = line.split(whichOperation)
+        answer = int(data[0]) + int(data[1])
+    elif whichOperation == "-":
+        data = line.split(whichOperation)
+        answer = int(data[0]) - int(data[1])
+    elif whichOperation == "*":
+        data = line.split(whichOperation)
+        answer = int(data[0]) * int(data[1])
+    elif whichOperation == "/":
+        data = line.split(whichOperation)
+        answer = int(data[0]) / int(data[1])
+    else:
+        print("This operation is not understood")
+        exit(1)
 else:
-    print("This operation is not understood")
-    exit(1)
+    print("Go Home")
+    exit(2)
 print(line, "=", answer)
