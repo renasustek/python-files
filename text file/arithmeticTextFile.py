dcaae7c8202cafb3322b758d750f50f4dd33e0e6
renasustek <renasustek@gmail.com>
2018-04-19 20:40:23 +0100

    Read operation from file

--- text file/arithmeticTextFile.py
+++ text file/arithmeticTextFile.py
@@ -1,10 +1,12 @@
-# file = open("arithmetic.txt", "w")
-#
-# number1 = input("Enter number 1: ")
-# number2 = input("Enter number 2: ")
-# file.write(number1 + "+" + number2)
-#
-# file.close()
+file = open("arithmetic.txt", "w")
+
+whichOperation = input("Which Operation do you want: ")
+
+number1 = input("Enter number 1: ")
+number2 = input("Enter number 2: ")
+file.write(number1 + whichOperation + number2)
+
+file.close()
 
 file = open("arithmetic.txt", "r")
 line = file.readline()
@@ -12,10 +14,21 @@ answer = None
 
 file.close()
 
-if "+" in line:
-    data = line.split("+")
+if whichOperation in line and whichOperation=="+":
+    data = line.split(whichOperation)
     answer = int(data[0]) + int(data[1])
+elif whichOperation in line and whichOperation=="-":
+    data = line.split(whichOperation)
+    answer = int(data[0]) - int(data[1])
+elif whichOperation in line and whichOperation=="*":
+    data = line.split(whichOperation)
+    answer = int(data[0]) * int(data[1])
+elif whichOperation in line and whichOperation=="/":
+    data = line.split(whichOperation)
+    answer = int(data[0]) / int(data[1])
+
+
 else:
-    print("Error: This operation is not understood")
+    print("This operation is not understood")
     exit(1)
 print(line, "=", answer)
